967056b48c4c3e844939ddeea3eee44ecd24bc49
djoudi-abdallah <abdellahdjoudi17@gmail.com>
2025-08-07 21:22:26 +0100

    Initial commit - Blog Backend with NestJS + Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { TagsModule } from './tags/tags.module';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, PostsModule, CommentsModule, TagsModule, UsersModule],
+  imports: [AuthModule, PostsModule, CommentsModule, TagsModule, UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
